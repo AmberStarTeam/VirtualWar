a3be754049642f59642f1dbbc7b0a9ede8b76368
beaussart <nico.nouvelle.adresse@gmail.com>
2015-04-08 22:44:55 +0200

    Ajout de quelques valeurs pour montrer l'usage

--- src/org/amberstar/virtualwar/TextData.java
+++ src/org/amberstar/virtualwar/TextData.java
@@ -1,8 +1,18 @@
 package org.amberstar.virtualwar;
 
 public enum TextData {
-	TANK_NAME("tank"), SCAVENGER_NAME("piegeur"), SHOOTER_NAME("tireur"), ERROR_MOVE_ISOBSTACLE(
-			"Déplacement imposible : il y a un obstacle");
+	TANK_NAME("tank"),
+	SCAVENGER_NAME("piegeur"),
+	SHOOTER_NAME("tireur"),
+	ERROR_MOVE_IS_OBSTACLE("Déplacement imposible : il y a un obstacle !"),
+	ERROR_MOVE_IS_OCCUPED("Déplacement imposible : il y a déjà un robot présent !"),
+	ERROR_MOVE_IS_MINE("Déplacement imposible : il y a une mine de votre équipe !"),
+	ERROR_MOVE_IS_OUT_OF_BOUND("Déplacement imposible : ce n'est pas sur le plateau !"),
+	ERROR_MOVE_IS_BASE_OTHER("Déplacement imposible : ce n'est pas votre base !"),
+	ERROR_MOVE_IS_NOT_VALID("Déplacement imposible : ce n'est pas un déplacement possible pour cette unitée !"),
+	ERROR_BOARD_TOO_SMALL("Création impossible : le plateau est trop petit !"),
+	ERROR_BOARD_TOO_BIG("Création impossible : le plateau est trop grand !"),
+	ERROR_BOARD_MUST_BE_POSITIF("Création impossible : les valeurs doivent être positives");
 
 	private String name = "";
 
