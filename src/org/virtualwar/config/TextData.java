7ede30f8dcb6ba8f335c4828b3fee9406e3a1779
MoulardS <fappingmaterial@hotmail.fr>
2015-04-16 17:00:33 +0200

    Correction orthographe

--- src/org/virtualwar/config/TextData.java
+++ src/org/virtualwar/config/TextData.java
@@ -24,31 +24,31 @@ public enum TextData {
 
 	/** The label regle. */
 	LABEL_REGLE(
-			"\tRègles du jeu :\n"
+			"\tRÃ¨gles du jeu :\n"
 					+ "Les pays s'affrontent en faisant combattre des robots sur un plateau.\n\n"
-					+ "Le jeu se déroule au tour par tour."
-					+ " L'équipe qui commence est choisie aléatoirement.\n\n"
+					+ "Le jeu se dÃ©roule au tour par tour."
+					+ " L'Ã©quipe qui commence est choisie alÃ©atoirement.\n\n"
 					+ "Les robots commencent dans leur base respective.\n\n"
-					+ "A chaque tour, chaque équipe choisit"
-					+ " un de ses robots pour réaliser une de ses actions.\n\n"
-					+ "Une équipe ne peut pas passer son tour.\n"
+					+ "A chaque tour, chaque Ã©quipe choisit"
+					+ " un de ses robots pour rÃ©aliser une de ses actions.\n\n"
+					+ "Une Ã©quipe ne peut pas passer son tour.\n"
 					+ "Elle doit jouer tant que ses robots ont"
-					+ " assez d'énergie pour réaliser une action.\n\n"
-					+ "Au cours de la partie chaque équipe doit"
+					+ " assez d'Ã©nergie pour rÃ©aliser une action.\n\n"
+					+ "Au cours de la partie chaque Ã©quipe doit"
 					+ " conserver au moins un robot hors de sa base.\n\n"
-					+ "La partie se termine dès que l'un des deux"
-					+ " partis ne possède plus de robot vivant.\n\n"
+					+ "La partie se termine dÃ¨s que l'un des deux"
+					+ " partis ne possÃ¨de plus de robot vivant.\n\n"
 					+ "\t Comment jouer ? : \n\n"
 					+ "A) Les robots : \n\n"
-					+ "Tireur : Robot léger, capable de tirer sur une courte distance."
-					+ " Peut se déplacer dans tout les sens.\n\n"
-					+ "Piégeur : Poseur de mine. Peut se déplacer dans tous les sens.\n\n"
+					+ "Tireur : Robot lÃ©ger, capable de tirer sur une courte distance."
+					+ " Peut se dÃ©placer dans tout les sens.\n\n"
+					+ "PiÃ©geur : Poseur de mine. Peut se dÃ©placer dans tous les sens.\n\n"
 					+ "Char : Robot lourd, capable de tirer sur de longues distances."
-					+ " Ne peut pas se déplacer en diagonale.\n\n\n"
+					+ " Ne peut pas se dÃ©placer en diagonale.\n\n\n"
 					+ "B) Bases et energie : \n\n"
-					+ "Chaque action coûte de l'énergie."
-					+ "Quand un robot n'a plus d'énergie, il ne peut plus jouer.\n\n"
-					+ "Quand il retourne dans sa base, un robot récupère peu à peu son énergie."),
+					+ "Chaque action coÃ»te de l'Ã©nergie."
+					+ "Quand un robot n'a plus d'Ã©nergie, il ne peut plus jouer.\n\n"
+					+ "Quand il retourne dans sa base, un robot rÃ©cupÃ¨re peu Ã  peu son Ã©nergie."),
 
 	/** The label up. */
 	LABEL_UP("haut", "up"),
@@ -63,19 +63,19 @@ public enum TextData {
 	LABEL_LEFT("gauche", "left"),
 
 	/** The label up right. */
-	LABEL_UP_RIGHT(LABEL_UP.get("fr") + " à " + LABEL_RIGHT.get("fr"), LABEL_UP
+	LABEL_UP_RIGHT(LABEL_UP.get("fr") + " Ã  " + LABEL_RIGHT.get("fr"), LABEL_UP
 			.get("en") + " " + LABEL_RIGHT.get("en")),
 
 	/** The label up left. */
-	LABEL_UP_LEFT(LABEL_UP.get("fr") + " à " + LABEL_LEFT.get("fr"), LABEL_UP
+	LABEL_UP_LEFT(LABEL_UP.get("fr") + " Ã  " + LABEL_LEFT.get("fr"), LABEL_UP
 			.get("en") + " " + LABEL_LEFT.get("en")),
 
 	/** The label down right. */
-	LABEL_DOWN_RIGHT(LABEL_DOWN.get("fr") + " à " + LABEL_RIGHT.get("fr"),
+	LABEL_DOWN_RIGHT(LABEL_DOWN.get("fr") + " Ã  " + LABEL_RIGHT.get("fr"),
 			LABEL_DOWN.get("en") + " " + LABEL_RIGHT.get("en")),
 
 	/** The label down left. */
-	LABEL_DOWN_LEFT(LABEL_DOWN.get("fr") + " à " + LABEL_LEFT.get("fr"),
+	LABEL_DOWN_LEFT(LABEL_DOWN.get("fr") + " Ã  " + LABEL_LEFT.get("fr"),
 			LABEL_DOWN.get("en") + " " + LABEL_LEFT.get("en")),
 
 	/** The label tank team a. */
@@ -112,7 +112,7 @@ public enum TextData {
 	LABEL_OBSTACLE("O"),
 
 	/** The label legende title. */
-	LABEL_LEGENDE_TITLE("Légende", "Legend"),
+	LABEL_LEGENDE_TITLE("LÃ©gende", "Legend"),
 
 	/** The tank name. */
 	TANK_NAME("Tank", "Tank"),
@@ -161,42 +161,42 @@ public enum TextData {
 	LABEL_LEGENDE_QUIT("q - quitter", "q - quit"),
 
 	/** The error move is obstacle. */
-	ERROR_MOVE_IS_OBSTACLE("Déplacement imposible : Il y a un obstacle !",
-			"Move impossible : There is a obstacle on the way !"),
+	ERROR_MOVE_IS_OBSTACLE("DÃ©placement imposible : Il y a un obstacle !",
+			"Move impossible : There is an obstacle on the way !"),
 
 	/** The error move is obstacle. */
 	ERROR_MOVE_NOT_ENOUTH_ENERGY(
-			"Déplacement imposible : Il n'a pas assez d'énergie",
-			"Move impossible : You don't have enouth energy !"),
+			"DÃ©placement imposible : Il n'a pas assez d'Ã©nergie",
+			"Move impossible : You don't have enough energy !"),
 
 	/** The error move is obstacle. */
 	ERROR_ATTACK_NOT_ENOUTH_ENERGY(
-			"Attaque imposible : Il n'a pas assez d'énergie",
-			"Attack impossible : You don't have enouth energy !"),
+			"Attaque imposible : Il n'a pas assez d'Ã©nergie",
+			"Attack impossible : You don't have enough energy !"),
 
 	/** The error nothing to attack. */
 	ERROR_ATTACK_NOTHING_TO_ATTACK(
-			"Attaque imposible : Il n'y a rien à attaquer",
+			"Attaque imposible : Il n'y a rien Ã  attaquer",
 			"Attack impossible : Nothing to attack"),
 
 	/** The error nothing to attack. */
 	ERROR_ATTACK_NOT_ENOUTH_MINE(
 			"Attaque imposible : Il n'a pas assez de mines",
-			"Attack impossible : You don't have enouth mine !"),
+			"Attack impossible : You don't have enough mine !"),
 
 	/** The error nothing to attack. */
-	ERROR_ATTACK_ALREADY_MINED("Attaque imposible : Il y a déja une mine !",
+	ERROR_ATTACK_ALREADY_MINED("Attaque imposible : Il y a dÃ©jÃ  une mine !",
 			"Attack impossible : There is already a mine here !"),
 
 	/** The error nothing to attack. */
 	ERROR_ATTACK_CANT_ATTACK_BASE(
-			"Attaque imposible : Tu ne peut pas attaquer une base",
+			"Attaque imposible : Tu ne peux pas attaquer une base",
 			"Attack impossible : You can't attack a base"),
 
 	/** The error nothing to attack. */
 	ERROR_ATTACK_CANT_FROM_BASE(
-			"Attaque imposible : Tu ne peut pas attaquer depuis base",
-			"Attack impossible : you can attack from your base"),
+			"Attaque imposible : Tu ne peux pas attaquer depuis base",
+			"Attack impossible : You can't attack from your base"),
 
 	/** The error nothing to attack. */
 	ERROR_ATTACK_CANT_MINE_BASE(
@@ -210,7 +210,7 @@ public enum TextData {
 
 	/** The error nothing to attack. */
 	ERROR_ATTACK_CANT_MINE_OBSTACLE("Mine imposible : Il y a un obstacle !",
-			"Mine impossible : It's a obstacle"),
+			"Mine impossible : It's an obstacle"),
 
 	/** The error move is out of bound. */
 	ERROR_ATTTACK_IS_OUT_OF_BOUND(
@@ -219,58 +219,58 @@ public enum TextData {
 
 	/** The robot has been shot. */
 	ERROR_MOVE_NOWHERE_TO_MOVE(
-			"Déplacement imposible : Pas d'endroit à aller !",
+			"DÃ©placement imposible : Pas d'endroit oÃ¹ aller !",
 			"Move impossible : Nowhere to go !"),
 
 	/** The error move is occupied. */
 	ERROR_MOVE_IS_OCCUPED(
-			"Déplacement imposible : Il y a déjà un robot présent !",
+			"DÃ©placement imposible : Il y a dÃ©jÃ  un robot prÃ©sent !",
 			"Move impossible : A robot is already there !"),
 
 	/** The error move is mine. */
 	ERROR_MOVE_IS_MINE(
-			"Déplacement imposible : Il y a une mine de votre équipe !",
-			"Move impossible : There are a mine of you're team !"),
+			"DÃ©placement imposible : Il y a une mine de votre Ã©quipe !",
+			"Move impossible : There is one of your team's mines !"),
 
 	/** The error move is out of bound. */
 	ERROR_MOVE_IS_OUT_OF_BOUND(
-			"Déplacement imposible : Ce n'est pas sur le plateau !",
+			"DÃ©placement imposible : Ce n'est pas sur le plateau !",
 			"Move impossible : It's not on the board !"),
 
 	/** The error move is base other. */
 	ERROR_MOVE_IS_BASE_OTHER(
-			"Déplacement imposible : Ce n'est pas votre base !",
+			"DÃ©placement imposible : Ce n'est pas votre base !",
 			"Move impossible : It's not your base !"),
 
 	/** The error move is not valid. */
 	ERROR_MOVE_IS_NOT_VALID(
-			"Déplacement imposible : Ce n'est pas un déplacement possible pour cette unitée !",
+			"DÃ©placement imposible : Ce n'est pas un dÃ©placement possible pour cette unitÃ© !",
 			"Move impossible : It's not a valid move for this robot !"),
 
 	/** The error board too small. */
-	ERROR_BOARD_TOO_SMALL("Création impossible : Le plateau est trop petit !",
-			"Creation impossible : the board is to small !"),
+	ERROR_BOARD_TOO_SMALL("CrÃ©ation impossible : Le plateau est trop petit !",
+			"Creation impossible : The board is too small !"),
 
 	/** The error board too big. */
-	ERROR_BOARD_TOO_BIG("Création impossible : Le plateau est trop grand !",
-			"Creation impossible : the board is too big !"),
+	ERROR_BOARD_TOO_BIG("CrÃ©ation impossible : Le plateau est trop grand !",
+			"Creation impossible : The board is too big !"),
 
 	/** The error board must be positive. */
 	ERROR_BOARD_MUST_BE_POSITIF(
-			"Création impossible : Les valeurs doivent être positives",
-			"Creation impossible : the values must be positive"),
+			"CrÃ©ation impossible : Les valeurs doivent Ãªtre positives",
+			"Creation impossible : The values must be positive"),
 
 	/** The robot has been shot. */
-	ROBOT_HAS_BEEN_SHOT(" a été touché !", "has been shoot"),
+	ROBOT_HAS_BEEN_SHOT(" a Ã©tÃ© touchÃ© !", "has been shoot"),
 
 	/** The robot has been mine. */
-	ROBOT_HAS_BEEN_MINE(" a été piégé !", "has been mined"),
+	ROBOT_HAS_BEEN_MINE(" a Ã©tÃ© piÃ©gÃ© !", "has been mined"),
 
 	/** The robot mine. */
-	ROBOT_MINE("Un piégeur a miné !", "A scavenger has mined !"),
+	ROBOT_MINE("Un piÃ©geur a minÃ© !", "A scavenger has mined !"),
 
 	/** The game move. */
-	GAME_MOVE("Déplacer", "Move"),
+	GAME_MOVE("DÃ©placer", "Move"),
 
 	/** The game attack. */
 	GAME_ATTACK("Attaquer", "Attack"),
@@ -284,11 +284,11 @@ public enum TextData {
 	/** The game cancel. */
 	GAME_CANCEL("Anuler", "Cancel"),
 	
-	GAME_MOVING_ON("Déplacemnet en ", "Moving on "),
+	GAME_MOVING_ON("DÃ©placement en ", "Moving on "),
 
 	/** The game board size. */
 	GAME_BOARD_SIZE(
-			"Choisissez la taille du plateau (elle ne doit pas être inférieur à 3)",
+			"Choisissez la taille du plateau (elle ne doit pas Ãªtre infÃ©rieure Ã  3)",
 			"Chose a board size (can't be inferior to 3)"),
 			
 	/** The game height. */
@@ -298,43 +298,43 @@ public enum TextData {
 	GAME_WIDTH("Largeur","Width"),
 	
 	/** The game is player x ia. */
-	GAME_IS_PLAYER_X_IA("Le joeur X est une ia ? ","Is the player X an ia?"),
+	GAME_IS_PLAYER_X_IA("Le joueur X est-il une ia? ","Is the player X an ia?"),
 	
 	/** The game chose ia input. */
 	GAME_CHOSE_IA_INPUT("o\nn" + '\u9999' + "(o) oui, (n) non", "y\nn" + '\u9999' + "(y) yes, (n) no"),
 	
 	/** The game obstacle. */
-	GAME_OBSTACLE("Pourcentage d'obstacle : ", "Percentage of obstacle : "),
+	GAME_OBSTACLE("Pourcentage d'obstacles : ", "Percentage of obstacle : "),
 	
 	/** The game turn player. */
 	GAME_TURN_PLAYER("Au tour de ", "It's the turn of "),
 
 	/** The game win team. */
-	GAME_WIN_TEAM("L'équipe XXX à gagner !", "The team XXX has won !"),
+	GAME_WIN_TEAM("L'Ã©quipe XXX Ã  gagner !", "The team XXX has won !"),
 
 	/** The game end. */
-	GAME_END("Appuillez sur une touche pour quiter", "Push any key to quit"),
+	GAME_END("Appuyez sur une touche pour quitter", "Push any key to quit"),
 
 	/** The game name team x. */
 	GAME_NAME_TEAM_X("Nom de la team X : ", "Name of the X team : "),
 
 	/** The game chose nmb bots. */
 	GAME_CHOSE_NMB_BOTS(
-			"Choisissez le nombre de robot pour les équipes (<6:) :",
+			"Choisissez le nombre de robots pour les Ã©quipes (<6:) :",
 			"Chose the number of robots for all teams (<6) :"),
 
 	/** The game invalid input. */
-	GAME_INVALID_INPUT("Entrée invalide ! ", "Invalid input !"),
+	GAME_INVALID_INPUT("EntrÃ©e invalide ! ", "Invalid input !"),
 
 	/** The game chose robots type. */
 	GAME_CHOSE_ROBOTS_TYPE("Choisissez le type de vos robots pour X : ",
-			"Chose the robots type for X : "),
+			"Chose the robots' type for X : "),
 
 	/** The error not number. */
 	ERROR_NOT_NUMBER("Ce n'est pas un nombre !", "Not a number !"),
 
 	/** The error team same name. */
-	ERROR_TEAM_SAME_NAME("Les deux équipes ont le même nom !",
+	ERROR_TEAM_SAME_NAME("Les deux Ã©quipes ont le mÃªme nom !",
 			"Both team have the same name !");
 
 	/** The french name. */
