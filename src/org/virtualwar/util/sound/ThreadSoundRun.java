491190fadb57534e174502049fcd18e260582851
beaussart <nico.nouvelle.adresse@gmail.com>
2015-04-18 12:59:11 +0200

    fixing sound not found exepction

--- src/org/virtualwar/util/sound/ThreadSoundRun.java
+++ src/org/virtualwar/util/sound/ThreadSoundRun.java
@@ -3,12 +3,12 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -74,7 +74,13 @@ public class ThreadSoundRun extends Thread {
 			return;
 		}
 		sound = new Sound(file, timeRunning);
-		is = new ByteArrayInputStream(sound.getSamples());
+		try {
+			is = new ByteArrayInputStream(sound.getSamples());
+		} catch (Exception e) {
+			sound = null;
+			return;
+		}
+
 	}
 
 	/**
