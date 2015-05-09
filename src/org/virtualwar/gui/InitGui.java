4ec8fd02e9b8d7bbb58cd2c8e08dad9de499c1fc
beaussart <nico.nouvelle.adresse@gmail.com>
2015-05-09 16:44:37 +0200

    Adding utilfunction for geting a jComboBox for ints

--- src/org/virtualwar/gui/InitGui.java
+++ src/org/virtualwar/gui/InitGui.java
@@ -8,8 +8,17 @@ import javax.swing.SwingUtilities;
 
 public class InitGui {
 
-	public static String getLangFromUser(){
-		String[] options = {"OK"};
+	public static JComboBox<String> getComboFromInt(int start, int end){
+		String[] lsInt = new String[end-start];
+		for (int i = 0; i < end-start; i++) {
+			lsInt[i] = "" + start+i;
+		}
+		
+		return new JComboBox<String>(lsInt);
+	}
+	
+	public static String getLangFromUser() {
+		String[] options = { "OK" };
 		JPanel panel = new JPanel();
 		JLabel lbl = new JLabel("Language : ");
 
@@ -18,27 +27,27 @@ public class InitGui {
 		JComboBox<String> boxLang = new JComboBox<String>(lsLang);
 		panel.add(lbl);
 		panel.add(boxLang);
-		int selectedOption = JOptionPane.showOptionDialog(null, panel, "Select your language", JOptionPane.NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options , options[0]);
+		int selectedOption = JOptionPane.showOptionDialog(null, panel,
+				"Select your language", JOptionPane.NO_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
-		if(selectedOption == 0)
-		{
-		    String text = (String)boxLang.getSelectedItem();
-		    return text.toLowerCase().substring(0, 2);
-		    
+		if (selectedOption == 0) {
+			String text = (String) boxLang.getSelectedItem();
+			return text.toLowerCase().substring(0, 2);
 		}
 		return null;
 	}
-	
+
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
-		    @Override
+			@Override
 			public void run() {
-		    	System.out.println(getLangFromUser());
-		        new InitGui();
-		    }
+				System.out.println(getLangFromUser());
+				new InitGui();
+			}
 		});
 	}
-	
+
 	public InitGui() {
 		// TODO Auto-generated constructor stub
 	}
