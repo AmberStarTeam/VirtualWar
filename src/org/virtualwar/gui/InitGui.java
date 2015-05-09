582f7be1319c1063ec094f7fc840a68d5a055bc7
beaussart <nico.nouvelle.adresse@gmail.com>
2015-05-09 16:21:29 +0200

    making the language gui

---
+++ src/org/virtualwar/gui/InitGui.java
@@ -0,0 +1,46 @@
+package org.virtualwar.gui;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class InitGui {
+
+	public static String getLangFromUser(){
+		String[] options = {"OK"};
+		JPanel panel = new JPanel();
+		JLabel lbl = new JLabel("Language : ");
+
+		String[] lsLang = { "Français", "English" };
+
+		JComboBox<String> boxLang = new JComboBox<String>(lsLang);
+		panel.add(lbl);
+		panel.add(boxLang);
+		int selectedOption = JOptionPane.showOptionDialog(null, panel, "Select your language", JOptionPane.NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options , options[0]);
+
+		if(selectedOption == 0)
+		{
+		    String text = (String)boxLang.getSelectedItem();
+		    return text.toLowerCase().substring(0, 2);
+		    
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+		    @Override
+			public void run() {
+		    	System.out.println(getLangFromUser());
+		        new InitGui();
+		    }
+		});
+	}
+	
+	public InitGui() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
