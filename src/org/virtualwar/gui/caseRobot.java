c84fbe54cd3a81df59831a69e6829b353e3b9017
Obled Joël <obled.joel@gmail.com>
2015-05-10 20:40:38 +0200

    Mise à jour de CaseRobot : Barre de vie +

--- src/org/virtualwar/gui/caseRobot.java
+++ src/org/virtualwar/gui/caseRobot.java
@@ -1,13 +1,14 @@
 package org.virtualwar.gui;
 
 import java.awt.BorderLayout;
-
+import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
-import org.virtualwar.board.Base;
 import org.virtualwar.board.Board;
 import org.virtualwar.board.Case;
 import org.virtualwar.board.Cell;
@@ -18,105 +19,121 @@ import org.virtualwar.robot.Tank;
 import org.virtualwar.util.Coordinates;
 
 /**
-* Classe qui créer un JPanel avec une image correspondant à un type de robot spécifié par le paramètre du constructeur ou un JPanel vide.
-* A venir : Barre de vie.
-* 
-* @author OBLED Joël
-**/
-
+ * Classe qui cr�er l'affichage d'une case selon son type (Base, obstacle, mine, robots).
+ * 
+ * @author Jo�l OBLED
+ *
+ */
 
 @SuppressWarnings("serial")
-public class caseRobot extends JFrame {	
-	
-	//Constructeur utilisant un robot entré en paramètre
-	public caseRobot(Robot robot) {
-		this.setSize(10,10);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-		this.add(initRobot(robot));
-		this.pack();
-		this.setVisible(true);
-	}
-	
+public class caseRobot extends JFrame {
+
+	private int hp;
+
+	// Constructeur utilisant une case entree en parametre
 	public caseRobot(Cell cell) {
-		this.setSize(10,10);
+		this.setSize(10, 10);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
-		this.add(initObstacle(cell));
+		this.add(initCase(cell));
 		this.pack();
 		this.setVisible(true);
 	}
-	
-	//Création d'un JPanel contenant l'image correspondant au type de Robot en paramètre
-	//ou, le cas échéant, création d'un JPanel vide.
-	public JPanel initRobot(Robot type) {
-		if(type.getType().equals("Tank")) {
-			JPanel tank = new JPanel();
-			JLabel iTank = new JLabel(new ImageIcon("pictures/Tank-icon.png"));
-			tank.add(iTank, BorderLayout.CENTER);
-			return tank;
-		}else if(type.getType().equals("Shooter")) {
-			JPanel shooter = new JPanel();
-			JLabel iShooter = new JLabel(new ImageIcon("pictures/Shooter-icon.png"));
-			shooter.add(iShooter, BorderLayout.CENTER);
-			return shooter;
-		}else if(type.getType().equals("Scavenger")) {
-			JPanel scavenger = new JPanel();
-			JLabel iScavenger = new JLabel(new ImageIcon("pictures/Scavenger-icon.png"));
-			scavenger.add(iScavenger, BorderLayout.CENTER);
-			return scavenger;
-		}else{
-			JPanel vide = new JPanel();
-			return vide;
-		}
-	}
-	
-	//Création d'un JPanel contenant l'image correspondant au type de case en paramètre
-	//ou, le cas échéant, création d'un JPanel vide.
-	public JPanel initObstacle(Cell cell) {
-		if(cell.isBase() > 0) {
+
+	// Creation d'un JPanel contenant l'image correspondante au type de case en 
+	// parametre : base, obstacle, mine ou le type de robot qu'elle contient avec sa barre de vie et/
+	// ou mine.
+	public JPanel initCase(Cell cell) {
+		if (cell.isBase() > 0) {
 			JPanel base = new JPanel();
 			JLabel iBase = new JLabel(new ImageIcon("pictures/Base-icon.png"));
 			base.add(iBase, BorderLayout.CENTER);
 			return base;
-		}else if(cell.isObstacle()) {
+		} else if (cell.isObstacle()) {
 			JPanel obstacle = new JPanel();
-			JLabel iObstacle = new JLabel(new ImageIcon("pictures/Obstacle-icon.png"));
+			JLabel iObstacle = new JLabel(new ImageIcon(
+					"pictures/Obstacle-icon.png"));
 			obstacle.add(iObstacle, BorderLayout.CENTER);
 			return obstacle;
-		}else if(cell.mineContains() > 0) {
+		} else if (cell.mineContains() > 0) {
 			JPanel mine = new JPanel();
 			JLabel iMine = new JLabel(new ImageIcon("pictures/Mine-icon.png"));
 			mine.add(iMine, BorderLayout.CENTER);
 			return mine;
-		}else{
+		} else if (cell.getRobotIn() != null) {
+			if (cell.getRobotIn() instanceof Tank) {
+				JPanel tank = new JPanel();
+				JLabel iTank = new JLabel(new ImageIcon(
+						"pictures/Tank-icon.png"));
+				tank.add(iTank, BorderLayout.WEST);
+				tank.add(etatVie(cell.getRobotIn()), BorderLayout.EAST);
+				return tank;
+			} else if (cell.getRobotIn() instanceof Shooter) {
+				JPanel shooter = new JPanel();
+				JLabel iShooter = new JLabel(new ImageIcon(
+						"pictures/Shooter-icon.png"));
+				shooter.add(iShooter, BorderLayout.WEST);
+				shooter.add(etatVie(cell.getRobotIn()), BorderLayout.EAST);
+				return shooter;
+			} else if (cell.getRobotIn() instanceof Scavenger) {
+				JPanel scavenger = new JPanel();
+				JLabel iScavenger = new JLabel(new ImageIcon(
+						"pictures/Scavenger-icon.png"));
+				scavenger.add(iScavenger, BorderLayout.WEST);
+				scavenger.add(etatVie(cell.getRobotIn()), BorderLayout.EAST);
+				scavenger.add(etatMine(cell.getRobotIn()), BorderLayout.EAST);
+				return scavenger;
+			}
+		} else {
 			JPanel vide = new JPanel();
 			return vide;
 		}
+		return null;
 	}
-	
+
+	// Creation de la barre de vie selon l'energie du type de Robot
+	public JProgressBar etatVie(Robot robot) {
+		JProgressBar jHP = new JProgressBar(JProgressBar.VERTICAL, 0, robot.getMaxEng());
+		jHP.setForeground(Color.red);
+		jHP.setValue(robot.getMaxEng());
+		Dimension size = jHP.getPreferredSize();
+		size.width = 10;
+		size.height = 60;
+		jHP.setPreferredSize(size);
+		return jHP;
+	}
+
+	// Creation de la barre de mine selon les mines actuelles du robot.
+	public JProgressBar etatMine(Robot robot) {
+		JProgressBar jMine = new JProgressBar(JProgressBar.VERTICAL, 0,
+				((Scavenger) robot).getStock());
+		jMine.setForeground(Color.yellow);
+		jMine.setValue(((Scavenger) robot).getStock());
+		Dimension size = jMine.getPreferredSize();
+		size.width = 10;
+		size.height = 60;
+		jMine.setPreferredSize(size);
+		return jMine;
+	}
+
 	public static void main(String[] args) {
+
+		// Test des differentes cases
+		//Robots
+		Robot tank = new Tank(1, new Coordinates(1, 1), Board.newBoard(10, 10));
+		Robot shooter = new Shooter(1, new Coordinates(1, 1), Board.newBoard(
+				10, 10));
+		Robot scavenger = new Scavenger(1, new Coordinates(1, 1),
+				Board.newBoard(10, 10));
+		Cell test = new Case(new Coordinates(1,1));
 		
-		//Test des différents JPanel robot
-		Robot tank = new Tank(1, new Coordinates(1,1), Board.newBoard(10, 10));
-		Robot shooter = new Shooter(1, new Coordinates(1,1), Board.newBoard(10,10));
-		Robot scavenger = new Scavenger(1, new Coordinates(1,1), Board.newBoard(10,10));
+		//Pour tester un robot
+		test.setRobotIn(scavenger);
 		
-		//A tester un par un
-		//new caseRobot(tank);
-		//new caseRobot(shooter);
-		//new caseRobot(scavenger);
+		//Pour tester le reste
+		//test.setMine(1);
+		//test.setObstacle();
 		
-		//Test des différents JPanel case
-		Cell mine = new Case(new Coordinates(1,1));
-		mine.setMine(1);
-		Cell obstacle = new Case(new Coordinates(1,1));
-		obstacle.setObstacle();
-		Cell base = new Base(new Coordinates(1,1),1);
-		
-		//A tester un par un
-		//new caseRobot(mine);
-		//new caseRobot(obstacle);
-		//new caseRobot(base);
+		new caseRobot(test);
 	}
 }
